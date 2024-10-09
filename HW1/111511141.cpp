74fda1f6d01f1bcd499ea9a5d8b769797de2698d
Ken0221 <85241458+Ken0221@users.noreply.github.com>
2024-10-09 21:41:59 +0800

    finish V1

---
+++ HW1/111511141.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void sort(vector<int> &arr) {
+    int len = arr.size();
+    for (int i = 0; i < len; i++) {
+        for (int j = i + 1; j < len; j++) {
+            if (arr[i] > arr[j]) swap(arr[i], arr[j]);
+        }
+    }
+}
+
+int main() {
+    int power;
+    int score = 0;
+    vector<int> token;
+
+    cin >> power;
+    int token_in;
+    while (cin >> token_in && token_in != -1) {
+        token.push_back(token_in);
+    }
+
+    sort(token);
+
+    int max_score = 0;
+    int left = 0, right = token.size() - 1;
+    while (left <= right) {
+        // cout << "left: " << left << " right: " << right << endl;
+        if (power >= token[left]) { // if power can be used to gain score
+            power -= token[left];
+            score++;
+            if (score > max_score) max_score = score;
+            left++;
+        } else if (score > 0) { // otherwise, use score to gain power
+            power += token[right];
+            score--;
+            right--;
+        }
+    }
+
+    cout << max_score << endl;
+
+    return 0;
+}
